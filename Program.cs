07d90718edee8e8e471e5d1eba561c308bc5feca
gaspar <gaspares1978@gmail.com>
2018-11-30 07:19:28 -0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using tabuleiro;
+using xadrez;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirPartida(partida);
+
+
+                    Console.WriteLine();
+                    Console.Write("digite a origem:");
+                    Posicao origem = Tela.lerPosicaoXadrez().ToPosicao();
+                    partida.validarPosicaoDeorigem(origem);
+
+                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                    Console.Write("digite o destino:");
+                    Posicao destino = Tela.lerPosicaoXadrez().ToPosicao();
+                    partida.validarPosicaoDeDestino(origem, destino);
+
+                    partida.executaMovimento(origem, destino);
+                    partida.realizaJogada(origem, destino);
+                }
+
+                Console.Clear();
+                Tela.imprimirPartida(partida);
+
+
+
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
